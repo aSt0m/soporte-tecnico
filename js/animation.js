67a7008cb24a8831174c6d54afb2f93861eaf783
aSt0m <61126327+aSt0m@users.noreply.github.com>
2021-09-24 11:10:12 -0500

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -3,3 +3,4 @@ var time = gsap.timeline();
 time.from('#title', { duration: 1, x: -150 });
 time.from('#btn', { duration: 1.5, y: 100, opacity: 0 });
 time.from('#subtitle', { duration: 1, opacity: 0, ease: 'bounce' });
+// time.to('#img-car', { duration: 2, scale: 2, yoyo: true  },"-=0.5");
