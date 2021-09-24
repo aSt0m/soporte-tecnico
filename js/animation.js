db25f9462cad02384ab6b4801049be9dcffcfece
aSt0m <61126327+aSt0m@users.noreply.github.com>
2021-09-23 22:19:57 -0500

    Delete js directory

--- js/animation.js
+++
@@ -1,5 +0,0 @@
-var time = gsap.timeline();
-
-time.from('#title', { duration: 1, x: -150 });
-time.from('#btn', { duration: 1.5, y: 100, opacity: 0 });
-time.from('#subtitle', { duration: 1, opacity: 0, ease: 'bounce' });
